4f8ebec53de004647f574bfacce23ab7f74bb665
Chris Meyer <meyer.cm@gmail.com>
2016-12-26 23:52:39 -0500

    only a single ring of motors, seems to work decently

--- src/motors.cpp
+++ src/motors.cpp
@@ -3,16 +3,14 @@ namespace{
   const int MIN_POWER = 32;
   const int PERIOD = 20;
 
-  const float DIAG_1 = 0.4;
-  const float DIAG_2 = 0.72;
-  const float Z_OFFSET = 0.59;
-  const float X_LINE = 0.84;
-
   class Motor {
   public:
-    Motor(byte p_enable_pin, byte p_pwm_pin){
+    Motor(byte p_enable_pin, byte p_pwm_pin, float x, float y, float z){
       enable_pin = p_enable_pin;
       pwm_pin = p_pwm_pin;
+      x_coord = x;
+      y_coord = y;
+      z_coord = z;
     }
 
     void update(unsigned long now){
@@ -26,13 +24,13 @@ namespace{
       }
     }
 
-    void set_tone(int power, int duration, unsigned long now){
-      // This is temporary, and mostly driven by the fuckup with pin 9 / PWM_3
-      // whereby we can't really do pwm with that motor, so we fake it through the
-      // enable pin.
+    void set_tone(int x, int y, int z, int duration, unsigned long now){
+      // dot product, result is cos(theta) * magnitude:  0.0-255.0
+      float power = (x * x_coord) + (y * y_coord) + (z * z_coord);
       if (power > MIN_POWER){
         turn_off_time = now + duration;
-        power_level = (int)((power / 255.0) * PERIOD);
+        //normalize to PERIOD
+        power_level = (int)((power / (255.0)) * PERIOD);
         start_time = now;
         is_off = false;
       } else {
@@ -52,72 +50,38 @@ namespace{
     unsigned long start_time;
     int power_level;
     bool is_off = false;
+    float x_coord;
+    float y_coord;
+    float z_coord;
   };
 
   Motor *motors[] = {
-    new Motor(EN_1, PWM_1),
-    new Motor(EN_2, PWM_2),
-    new Motor(EN_3, PWM_3),
-    new Motor(EN_4, PWM_4),
-    new Motor(EN_5, PWM_5),
-    new Motor(EN_6, PWM_6),
+    new Motor(EN_1, PWM_1, -0.34, 0.8, 0.5), //120
+    new Motor(EN_2, PWM_2, -0.34, -0.8, 0.5), //240
+    // new Motor(EN_3, PWM_3), //180
+    new Motor(EN_4, PWM_4, -0.34, 0.0, -0.94), //0
+    // new Motor(EN_5, PWM_5), // 300
+    // new Motor(EN_6, PWM_6), //60
   };
+  int num_motors = 3;
 
-  // Motor num,  -1 for indicies
-  const byte POS_U = 6           - 1;
-  const byte NEG_U = 2           - 1;
-  const byte POS_V = 1           - 1;
-  const byte NEG_V = 5           - 1;
-  const byte POS_W = 4           - 1;
-  const byte NEG_W = 3           - 1;
-
-  const int PULSE_DURATION = 200;
+  const int PULSE_DURATION = 100;
 }
 
 void Motors::initialize() {
-  for(int i=0; i < 6; i++){
+  for(int i=0; i < num_motors; i++){
     motors[i]->turn_off();
   }
 }
 
 void Motors::set_tones(int x, int y, int z, unsigned long now){
-  int u = transform_u(x, y, z);
-  int v = transform_v(x, y, z);
-  int w = transform_w(x, y, z);
-
-  if (abs(u) > abs(v) && abs(u) > abs (w)){
-    if (u > 0) {
-      motors[POS_U]->set_tone(u, PULSE_DURATION, now);
-    } else {
-      motors[NEG_U]->set_tone(abs(u), PULSE_DURATION, now);
-    }
-  } else if (abs(v) > abs(u) && abs(v) > abs(w)){
-    if (v > 0) {
-      motors[POS_V]->set_tone(v, PULSE_DURATION, now);
-    } else {
-      motors[NEG_V]->set_tone(abs(v), PULSE_DURATION, now);
-    }
-  } else{
-    if (w > 0) {
-      motors[POS_W]->set_tone(w, PULSE_DURATION, now);
-    } else {
-      motors[NEG_W]->set_tone(abs(w), PULSE_DURATION, now);
-    }
+  for(int i=0; i < num_motors; i++){
+    motors[i]->set_tone(x, y, z, PULSE_DURATION, now);
   }
 }
 
 void Motors::update_motors(unsigned long now){
-  for(int i=0; i < 6; i++){
+  for(int i=0; i < num_motors; i++){
     motors[i]->update(now);
   }
-}
-
-int Motors::transform_u(int x, int y, int z){
-  return (int)(DIAG_1 * x + DIAG_2 * y + Z_OFFSET * z);
-}
-int Motors::transform_v(int x, int y, int z){
-  return (int)(-DIAG_1 * x + DIAG_2 * y + -Z_OFFSET * z);
-}
-int Motors::transform_w(int x, int y, int z){
-  return (int)(-X_LINE * x + 0 * y + Z_OFFSET * z);
 }
--- src/motors.hpp
+++ src/motors.hpp
@@ -8,9 +8,6 @@ public:
   void initialize();
   void set_tones(int x, int y, int z, unsigned long time);
   void update_motors(unsigned long time);
-  int transform_u(int x, int y, int z);
-  int transform_v(int x, int y, int z);
-  int transform_w(int x, int y, int z);
 };
 
 #endif
